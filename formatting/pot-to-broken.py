1dab7ce4b5b33140818ed67832c378ad0cb631de
Cian <guineec@tcd.ie>
2018-10-17 19:33:09 +0000

    Update results formatter to reformat pkb hashes

--- formatting/pot-to-broken.py
+++ formatting/pot-to-broken.py
@@ -21,7 +21,15 @@ print("Output file " + hash_path.replace(".hashes", ".broken") + " generated.")
 
 with open(output_path, "w+") as of, open(pot_path, "r") as potf:
     for line in potf:
-        formatted_line = line.replace(":", " ")
+        formatted_line = line.split(":")
+        pass_part = formatted_line[-1]
+        hash_part = ":".join(formatted_line[:-1])
+
+        if hash_part.startswith("sha256:2900"):
+            hash_part = hash_part.replace("sha256", "$pbkdf2-sha256")
+            hash_part = hash_part.replace(":", "$")
+
+        formatted_line = hash_part + " " + pass_part
         of.write(formatted_line)
 
 print("Formatted output written.")
